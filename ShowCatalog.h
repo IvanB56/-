a9b5fe31977229db2d1ca4da9bc0342c6dff32af
IvanB56 <61977172+IvanB56@users.noreply.github.com>
2020-05-01 18:51:01 +0500

    Add files via upload

--- ShowCatalog.h
+++ ShowCatalog.h
@@ -1,3 +1,7 @@
+#ifndef SHOW_CATALOG
 #define SHOW_CATALOG
-void ShowCatalog(std::vector<std::filesystem::path> &content, std::filesystem::path &Path,
-	unsigned short &beg, unsigned short &end);
+void ShowCatalog(std::vector<std::filesystem::path>& content, std::filesystem::path& Path,
+	unsigned short& beg, unsigned short& end);
+#endif // !SHOW_CATALOG
+
+
